57106da730e549e44219b4fe087e9e65613a6ac2
Rahman Younus <rahman.younus@dazn.com>
2019-11-20 10:13:56 +0000

    Initial commit - added basic files

--- src/main.tf
+++ src/main.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-  region      = "eu-west-2"
-  max_retries = 1
-}
-
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
---
+++ src/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type    = "string"
+  default = "eu-west-2"
+}
+
+variable "aws_profile" {
+  type = "string"
+  default = "default"
+}
+
+variable "aws_max_retries" {
+  type = "number"
+  default = 1
+}
